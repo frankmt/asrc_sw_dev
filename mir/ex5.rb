5276c69bafdc28f5e89ffea328ee2b1bf197139a
Mir Bugti <Mir Bugti>
2016-03-03 20:06:22 +1100

    Oh dear

---
+++ mir/ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie in 2009
+my_height = 74 # inches 
+my_weight = 180 # lbs
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+
+puts "let's talk about #{my_name}."
+puts "he's #{my_height} inches tall."
+puts "he's #{my_weight} pounds heavy."
+puts "actually that's not too heavy."
+puts "he's got #{my_eyes} eyes and #{my_hair} hair."
+puts "his teeth are usually #{my_teeth} depending on the coffee."
+
+
+puts "if i add #{my_age}, #{my_height}, and #{my_weight} i get #{my_age + my_height + my_weight}."
