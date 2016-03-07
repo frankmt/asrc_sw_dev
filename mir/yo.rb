3a89886bce11d31011bb9e0a4f0cfa1df685b678
Mir Bugti <Mir Bugti>
2016-03-07 19:55:20 +1100

    seventh March 2016

--- mir/yo.rb
+++ mir/yo.rb
@@ -1,3 +1,5 @@
+guess_is_wrong = true
+while guess_is_wrong 
 user_name = ARGV.first #
 prompt = '>'
 lucky_number = 7
@@ -9,11 +11,13 @@ number = $stdin.gets.chomp
 
 if number.to_i == lucky_number
 puts "Lucky Basterd"
-
+puts "your final guess was #{number}"
+guess_is_wrong = false
 else
 "Think harder"
 puts prompt
 puts "Second chance"
-number = $stdin.gets.chomp
+
 end
-puts "your final guess was #{number}"
+
+end
