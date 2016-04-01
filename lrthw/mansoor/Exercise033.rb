3921489cca8ae096fe9c65189d13066157f479d4
Mansoor Ali <MansoorAli@Mansoors-MacBook-Pro.local>
2016-04-01 22:11:03 +1100

    Ex.38

--- lrthw/mansoor/Exercise033.rb
+++ lrthw/mansoor/Exercise033.rb
@@ -3,10 +3,9 @@ letter = ARGV[0]
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 i = alphabet.index(letter)
-while i > 0
-
-       i -= 1
+while i < alphabet.size 
        letter =alphabet[i]
        puts letter
+       i += 1
    
 end
