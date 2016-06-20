2a9e9d3b8ade0d9194b046e9671490042715b75a
npostolovski <npostolovski@gmail.com>
2016-06-20 13:16:24 +1000

    Tash: Added feedback notes.

--- lrthw/mansoor/tash_challenges/Challenge4_fixed.rb
+++ lrthw/mansoor/tash_challenges/Challenge4_fixed.rb
@@ -1,8 +1,11 @@
 class Zoo
+  # You only need an attr_reader for the :name, because it's the only variable
+  # you use in the initialize. We'll go over during class on Thursday.
   attr_reader :name, :population, :health
 
   def initialize(name)
     @name = name
+    # The below variables are for the enclosure, not the zoo. They aren't needed.
     @population = population
     @health = health
   end
@@ -16,7 +19,9 @@ class Zoo
   end
 end
 
-zoo = Zoo.new('San Diego')
+# I have changed the zoo name, but the output is not updated. Can you fix it?
+# Then the challenge will be complete!
+zoo = Zoo.new('Melbourne')
 zoo.welcome
 zoo.enclosure("Lion", 7, "excellent")
 zoo.enclosure("Polar Bear", 1, "bad")
