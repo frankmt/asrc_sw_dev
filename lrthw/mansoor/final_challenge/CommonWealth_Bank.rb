5c47aaa188840b39c93c99b588a92dbc27466b80
Mansoor ALi <mansoor.hussain776@gmail.com>
2016-09-15 19:00:21 +1000

    Add files via upload

--- lrthw/mansoor/final_challenge/CommonWealth_Bank.rb
+++ lrthw/mansoor/final_challenge/CommonWealth_Bank.rb
@@ -1,24 +1,30 @@
 class Account
 
-  attr_reader :name, :account_balance, :amount
+  attr_reader :name, :account_balance, :amount, :initial_balance, :withdrawl_all, :deposit_all, :transfer_all
   def initialize
     @name = ""
     @account_balance = ""
     @amount = ""
     @other_account = ""
+    @initial_balance = ""
+    @withdrawl_all = 0
+    @deposit_all = 0
+    @transfer_all = 0
   end
   def display
      puts "Thank you for choosing Commonwealth Bank.
           Please enter your name:"
         @name = gets.chomp
      puts "please enter your opening_balance:"
-        @account_balance = gets.chomp
+        @initial_balance = gets.chomp
+        @account_balance = @initial_balance
      if @account_balance == "0"
-          puts main_menu
+     puts main_menu
     else
         @account_balance = @account_balance.to_i
-      if @account_balance != 0
-            puts main_menu
+    if @account_balance != 0
+     puts "Thank you #{@name}, your bank account has been created."
+        main_menu
       else
            puts "Please enter a number."
       end
@@ -26,13 +32,12 @@ class Account
   end
 
   def main_menu
-    puts "Thank you #{@name}, your bank account has been created.
-          What would you like to do?
+    puts "What would you like to do?
           1. Withdraw funds.
           2. Deposit funds.
           3. Transfer to another account.
-          4. View bank statement."
-
+          4. View bank statement.
+          5. Exit."
        option = gets.chomp
 
     case option
@@ -44,7 +49,7 @@ class Account
           transfer_to_another_account(amount)
       when "4"
           view_bank_statement(amount)
-     else
+      when "5"
           exit
      end
   end
@@ -54,10 +59,11 @@ class Account
          @amount = gets.chomp
          @amount = @amount.to_i
       if @amount >= @account_balance
-          puts "you can not withdrawl more than your available balance of $#{@account_balance}"
+         puts "you can not withdrawl more than your available balance of $#{@account_balance}"
       elsif @account_balance -= @amount
-         "You have withdrawl $#{@amount}: your balance is now $#{@account_balance}"
-
+         puts "You have withdrawl $#{@amount}: your balance is now $#{@account_balance}"
+         @withdrawl_all += @amount
+          main_menu
      end
    end
 
@@ -67,8 +73,8 @@ class Account
        @amount = gets.chomp
        @amount = @amount.to_i
     if @account_balance += @amount
-       "You have deposit $#{@amount}: your balance is now $#{@account_balance}"
-    else
+       puts "You have deposit $#{@amount}: your balance is now $#{@account_balance}"
+       @deposit_all += @amount
        main_menu
     end
   end
@@ -85,27 +91,27 @@ class Account
         puts "Which account do you want to transfer to?"
           @other_account = gets.chomp
           @account_balance -= @amount
-        "You have transfer $#{@amount} to #{@other_account} account: your balance is now $#{@account_balance}"
+        puts "You have transfer $#{@amount} to #{@other_account} account: your balance is now $#{@account_balance}"
+         @transfer_all += @amount
+          main_menu
     end
   end
 
   def view_bank_statement(amount)
 
-        print
-        "Bank Account Statement for #{@name}
+  print "Bank Account Statement for #{@name}
         ===========================================
-        Initial Deposit:
-        withdrawl_funds:
-        deposit_funds:
-        transfer_to_another_account:
+        Initial Deposit: #{@initial_balance}
+        withdrawl_all: #{@withdrawl_all}
+        deposit_all: #{@deposit_all}
+        transfer_all: #{@transfer_all}
 
         Balance: #{@account_balance}
 
-
-  Thank you for your time and expertise. Please contact me if you have any questions or need to clarify any of these requirements.
+  Thank you for your time and expertise.
+  Please contact me if you have any questions or need to clarify any of these requirements.
 
   All My Best
-
   Natasha Postolovski
   Chief Technology Officer
   Commonwealth Bank of Australia"
